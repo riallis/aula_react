7ec21f94176bf9549add3f34f23106d01f3df04b
Riallis <riallis.franca@gmail.com>
2018-07-08 19:50:26 -0300

    meu primeiro teste

---
+++ ex/he.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class he extends Component {
+  render() {
+    return (
+      <div>
+        <table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td> 
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td> 
+    <td>94</td>
+  </tr>
+</table>
+      </div>
+    )
+  }
+}
+
+export default he;
--- ex/index.jsx
+++ ex/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Field from './field'
+import He from './he'
 ReactDOM.render(
   <Field initialValue='Teste' />  
 , document.getElementById('app'))
 
+ReactDOM.render(<He/>, document.getElementById('mais'))
